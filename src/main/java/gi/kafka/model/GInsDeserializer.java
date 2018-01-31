d8a7d8fc23eb5b422dad4f8d992a496ab3e9fdea
Thomy <emps.ps.co@gmail.com>
2018-01-31 11:19:17 +0100

    GinsDeserializer example

--- src/main/java/gi/kafka/model/GInsDeserializer.java
+++ src/main/java/gi/kafka/model/GInsDeserializer.java
@@ -1,29 +1,58 @@
 package gi.kafka.model;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.FloatDeserializer;
 
+import gi.kafka.model.messages.VariableHeader;
+
 public class GInsDeserializer<T> implements Deserializer<T> {
 
 	public void close() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public void configure(Map<String, ?> configs, boolean isKey) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public T deserialize(String topic, byte[] data) {
+		GInsData ginsData = null;
 		try {
-			final GInsData ginsData = new GInsData(data);
+			// initialize GInsData with byte array or file name
+			ginsData = new GInsData(data);
+			
+			// obtain meta information and variable list
+			final GInsDataMetaModel meta = ginsData.getMeta();
+			final List<VariableHeader> variables = meta.getVariables();
+			
+			// select variable by index
+			final VariableHeader myVar = variables.get(0);
+			
+			// get data in Number array (generic way)
+			myVar.getGenericData();
+			
+			// get float data
+			if (myVar.getDataType() == VariableHeader.DATA_TYPE_Float) {
+				try {
+					float[] floatVals = myVar.getFloatData();
+				} catch (InvalidDataStreamException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		// couldn't read byte data
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
+		// free memory allocations in c++
+		} finally {
+			if (ginsData != null) {
+				ginsData.free();
+			}
 		}
 		
 		// TODO Auto-generated method stub
